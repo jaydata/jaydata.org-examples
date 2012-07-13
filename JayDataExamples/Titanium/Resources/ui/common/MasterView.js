be915fefdc0ba24ca7a49b563e060cfbe04c6918
Robert Bonay <robert.bonay@avangrade.com>
2012-07-13 10:47:30 +0300

    code comment

--- JayDataExamples/Titanium/Resources/ui/common/MasterView.js
+++ JayDataExamples/Titanium/Resources/ui/common/MasterView.js
@@ -3,8 +3,10 @@ function MasterView() {
 	
 	if (Ti.Platform.osname == 'android'){
 		Ti.include(Ti.Filesystem.resourcesDirectory + 'JayData-standalone.js');
+		//load the Netflix context definition
 		Ti.include(Ti.Filesystem.resourcesDirectory + 'netflix.js');	
-		
+
+		//inject the Titanium HTTPClient class to JayData in order to use it for requests (instead of XMLHttpRequest)
 		window.XMLHttpRequest = Ti.Network.HTTPClient;
 	}
 	
@@ -21,11 +23,24 @@ function MasterView() {
 			message: 'loading...'
 	});
 	ind.show();
-    					
+    	
+    	
+    	// This is how we retrieve the movie list from Netflix over OData protocol, using JayData
+	// The Netflix context an Title entity is defined in the netflix.js context definition
+	// JavaScript Language Query (JSLQ) syntax can be used to define what to retrieve with JayData
+	// more details: http://jaydata.org/blog/javascript-language-query-jslq-101
+	// The Netflix context definition configures the OData provider and set up the service URL
+	// 1. we want to retrieve records from the Titles entityset, which is contains the list of movies
+	// 2. orderByDescending - we tell the Netflix server to return the movies in descending order, ordered by the avarage rating field
+	// 3. take - we ask only for the TOP 50 records
+	
+	//JayData code begins here
 	Netflix.context.Titles
         .orderByDescending(function (movie) { return movie.AverageRating; })
-        .take(50)
-		.toArray(function(result) {
+        .take(50) 
+		.toArray(function(result) { 
+			//end of JayData query, now we have got the result from server without explicit AJAX calls 
+	    		//begin UI code
 			result.forEach(function (movie) {
 				var section = Ti.UI.createTableViewSection({ 
 					headerTitle: movie.Name + '( ' + movie.ReleaseYear + ' )'
