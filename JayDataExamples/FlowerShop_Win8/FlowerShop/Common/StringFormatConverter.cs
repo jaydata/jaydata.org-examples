be228f46b9e92bbc518f73e62b4af4124a083d7b
Peter Nochta <peter.nochta@jaydata.org>
2012-11-27 12:58:21 +0100

    Add App bar,

---
+++ JayDataExamples/FlowerShop_Win8/FlowerShop/Common/StringFormatConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace FlowerShop
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            // No format provided.
+            if (parameter == null)
+            {
+                return value;
+            }
+
+            return String.Format((String)parameter, value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
