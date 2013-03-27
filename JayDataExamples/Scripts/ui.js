8feef86da096f786617cb64bfab459410838ecbd
RynoRn <horvathlaszlo88@gmail.com>
2013-03-27 11:38:19 +0100

    Design upate.

--- JayDataExamples/Scripts/ui.js
+++ JayDataExamples/Scripts/ui.js
@@ -1,19 +1,16 @@
-//$(".tags-inline .tags .icon-remove").live('click', function () {
-//    $(this).parent(".label").parent("li").remove();
-//    console.log("do tag de-filtering here..");
-//});
+function toggleLogoFilter() {
+    $(".logo-filter").toggleClass("open");
+    $(".filter-icon").find(".z-up").toggleClass("hidden");
+    $(".filter-icon").find(".z-down").toggleClass("hidden");
+}
 
-//$(".well-white .tags .label").live('click', function () {
-    //console.log("do tag filtering here..");
-
-    //$(".tags-inline .tags").append("<li><a class='label label-purple' href='javascript:void(0)'>" + this.innerHTML + "<i class='icon-remove'></i></a></li>");
-//});
+$(document).on("click", ".main-content", function (e) {
+    if ($(".logo-filter").hasClass("open")) {
+        toggleLogoFilter();
+    }
+});
 
 $(".filter-icon").live('click', function (e) {
     e.preventDefault();
-
-    $(".logo-filter").toggleClass("open");
-
-    $(this).find(".z-up").toggleClass("hidden");
-    $(this).find(".z-down").toggleClass("hidden");
+    toggleLogoFilter();
 });
