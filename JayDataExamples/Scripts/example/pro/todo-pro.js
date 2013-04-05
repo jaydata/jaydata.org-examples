9959a748ebc1c6ebd664951fd802d2320237f7f2
Peter Nochta <peter.nochta@jaydata.org>
2013-04-05 11:42:42 +0200

    ToDo list with transaction

--- JayDataExamples/Scripts/example/pro/todo-pro.js
+++ JayDataExamples/Scripts/example/pro/todo-pro.js
@@ -19,30 +19,33 @@ $(function () {
 
     $('#btnAdd').click(function () {
         var value = $('#txtNew').val();
-        var id = $('#txtId').val();
         if (!value) return;
         var now = new Date();
-        var entity = new $todo.Types.ToDoEntry({ Value: value, CreatedAt: now, ModifiedAt: now });
-        if (id !== "") {
-            entity.Id = parseInt(id);
-        }
-        $todo.unCommitedItems.push(entity);
-        updateView();
-    });
-    $('#btnCommit').click(function () {
-        commit($todo.unCommitedItems.pop(), undefined);
-    });
-    $('#btnCommitTran').click(function () {
         $todo.context.beginTransaction(true, function (tran) {
-            commit($todo.unCommitedItems.pop(), tran);
+            $todo.context.TodoEntries.filter("it.Value == this.txt", { txt: value }).toArray({
+                success: function (items, innerTran) {
+                    if (items.length == 0) {
+                        var entity = new $todo.Types.ToDoEntry({ Value: value, CreatedAt: now, ModifiedAt: now });
+                        $todo.context.TodoEntries.add(entity);
+                        $todo.context.saveChanges(tran)
+                            .then(function (cnt, saveTran) { updateView(); })
+                            .fail(function () { alert("Save error all!"); updateView(); });
+                    } else {
+                        alert("Item is in the DB");
+                        updateView();
+                    }
+                },
+                error: function () { console.log("General error"); }
+            },
+            tran);
         });
     });
+
     $('#btnClear').click(function () {
         $('#todoList > div').each(function () {
             var entity = $(this).data('entity');
             $todo.context.TodoEntries.remove(entity);
         });
-        $todo.isError = false;
         $todo.context.saveChanges(updateView);
     });
 
@@ -62,34 +65,6 @@ $(function () {
         $todo.context.saveChanges(updateView);
     });
 
-    function commit(entity, transaction) {
-        saveEntity(entity, transaction)
-            .then(function (cnt, innerTran) {
-                if ($todo.unCommitedItems.length > 0) {
-                    commit($todo.unCommitedItems.pop(), undefined);
-                } else {
-                    $todo.isError = false;
-                    updateView();
-                }
-            })
-            .fail(function (innerTran) {
-                $todo.unCommitedItems = [];
-                $todo.isError = true;
-                console.log("ERROR");
-                updateView();
-            });
-    }
-
-    function saveEntity(entity, transaction) {
-        var def = new $.Deferred();
-        $todo.context.TodoEntries.add(entity);
-        $todo.context.saveChanges({
-            success: function () { def.resolve(arguments) },
-            error: function () { def.reject(arguments); }
-        }, transaction);
-        return def;
-    }
-
     function loadContext(options) {
         $todo.context = new $todo.Types.ToDoContext(options);
         $todo.context.onReady({
@@ -102,28 +77,17 @@ $(function () {
     }
 
     function updateView() {
-        if ($todo.isError) {
-            $('#error').show();
-        } else {
-            $('#error').hide();
-        }
         if ($todo.context) {
             $('#wrapper>div:not(#providerSelection)').show();
             $todo.context.TodoEntries.toArray(function (items) {
                 $('#todoList').empty();
                 items.forEach(function (entity) {
-                    console.log(entity._entityState);
                     $('#todoEntryTemplate').tmpl(entity).data('entity', entity).appendTo('#todoList');
-                });
-                $todo.unCommitedItems.forEach(function (entity) {
-                    console.log(entity._entityState);
-                    $('#todoTempEntryTemplate').tmpl(entity).data('entity', entity).appendTo('#todoList');
                 });
             });
         } else {
             $('#wrapper>div:not(#providerSelection)').hide();
         }
     }
-    $todo.unCommitedItems = $todo.unCommitedItems || [];
     updateView();
 });
